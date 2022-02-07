1b9dd374fa1d56a8d4971b4a4ede5e7245fdca93
karimjammoul <82494746+karimjammoul@users.noreply.github.com>
2022-02-07 23:52:50 +0200

    Pusher Events Listeners

--- resources/js/components/reply/Replies.vue
+++ resources/js/components/reply/Replies.vue
@@ -22,12 +22,27 @@ export default {
         EventBus.$on('newReply', (reply) => {
           this.question.replies.unshift(reply)
         })
+
         EventBus.$on('deleteReply', (index) => {
           axios.delete(`/api/question/${this.question.slug}/reply/${this.question.replies[index].id}`)
           .then(res => {
             this.question.replies.splice(index, 1)
           })
         })
+
+        Echo.private('App.Models.User.' + User.id())
+          .notification((notification) => {
+              this.question.replies.unshift(notification.reply)
+          });
+
+        Echo.channel('deleteReplyChannel')
+          .listen('DeleteReplyEvent', (e) => {
+            for(let i = 0; i < this.question.replies.length; i++) {
+              if(this.question.replies[i].id == e.id) {
+                this.question.replies.splice(i, 1)
+              }
+            }
+          })
       }
     }
 }
